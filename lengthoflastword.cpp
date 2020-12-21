8b86b2f0c3da854b51e941280fa738b69eab2d4d
CecilyK <cecily.kingori@strathmore.edu>
2020-12-21 18:22:05 +0300

    58

--- lengthoflastword.cpp
+++ lengthoflastword.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 
 int lengthOfLastWord(string s) {
-    int count=0;//counter for length
-    for(int i=s.size()-1;i>=0;--i)//traverse from right to left
+    int count=0;
+    for(int i=s.size()-1;i>=0;--i)//traverse from end
     {
-		if(s[i] !=' ')//count if not whitespace
+		if(s[i] ==' ' )//if space
 		{
-			count++;
-		}
-		else
-		break;
+            if(count !=0){//if count is not equal to 0
+                break;
+            }
+            continue;//if count = 0 continue means space is at the end
+            
+        }
+		count++;
 	}
 	return count;
 }
