8a17962b788469f2113288cab276f5e5bb13f4d6
CecilyK <cecily.kingori@strathmore.edu>
2021-01-05 15:25:24 +0300

    389

---
+++ findthedifference.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+char findTheDifference(string s, string t) {
+        char v = 0;
+        for (char c: s) v ^= c;
+        for (char c: t) v ^= c;
+        return v;
+    }
+    
+int main(){
+	string s,t;
+	cout<<"Enter string"<<endl;
+	cin>>s;
+	cin>>t;
+	cout<<findTheDifference(s,t);
+	return 0;
+}
+
